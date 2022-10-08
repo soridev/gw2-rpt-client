ec2d269c004af3ac808c04695cb280a10be8419f
Danni <daniel.ostermann1997@gmail.com>
2022-10-08 17:46:05 +0200

    init project from WPF-UI

---
+++ RPTClient/Models/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace RPTClient.Models
+{
+    public class AppConfig
+    {
+        public string ConfigurationsFolder { get; set; }
+
+        public string AppPropertiesFileName { get; set; }
+    }
+}
