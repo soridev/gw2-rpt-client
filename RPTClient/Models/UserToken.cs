534f14ad97f07be9e5002b5965c4f6a502d99044
Danni <daniel.ostermann1997@gmail.com>
2022-10-16 17:26:16 +0200

    add model for access token handling

---
+++ RPTClient/Models/UserToken.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPTClient.Models;
+
+public class UserToken
+{
+    private bool _firstUse = true;
+
+    private TimeSpan _useableTimespan = new TimeSpan(hours: 24, minutes:0, seconds:0);
+    public DateTime CreationTimestamp { get; set; }
+
+    private string _accessToken;
+    public string AccessToken()
+    {
+        if ((_firstUse is false) && (DateTime.Now < (CreationTimestamp + _useableTimespan)))
+        {
+            throw new Exception(String.Format("The currently used user token expired after {0}. Retry your action and a new token will be used.",
+                _useableTimespan.ToString()));
+        }
+        else
+        {
+            return _accessToken;
+        }
+    }
+    public void AccessToken(string accessToken)
+    {
+        _accessToken = accessToken;
+    }
+}
