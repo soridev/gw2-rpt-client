195fbbcdc49d577fb40931ca01a0e6eb29bddd99
Danni <daniel.ostermann1997@gmail.com>
2022-10-16 17:26:30 +0200

    add respository pattern class to access rest api

---
+++ RPTClient/Repositories/PerformanceTrackerRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using RPTClient.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPTClient.Repositories;
+
+public class PerformanceTrackerRepository
+{
+    private int _remoteLogCounter = 0;
+    private PerformanceTrackerRest _performanceTrackerRest = PerformanceTrackerRest.Instance;
+
+    public PerformanceTrackerRepository()
+    {
+        
+    }
+}
